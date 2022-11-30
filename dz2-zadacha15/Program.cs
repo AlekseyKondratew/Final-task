2317081aace8cdd72a9d5316942698e4b193d313
AlekseyKondratew <alekseykondratew@mail.ru>
2022-11-30 19:22:24 +0300

    Zadacha 15 reshena

---
+++ dz2-zadacha15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число обозначающее день недели");
+int a  = Convert.ToInt32(Console.ReadLine());
+    if (a < 6)
+    {
+    Console.WriteLine("Рабочий");
+    }
+    else
+    {
+        Console.WriteLine("Выходной");
+    }
+    
+
+
+
+
+
