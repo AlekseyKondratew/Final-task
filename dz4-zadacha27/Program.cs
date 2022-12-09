3f101174666a2bebe9493df813985fccce72c11e
AlekseyKondratew <alekseykondratew@mail.ru>
2022-12-09 11:31:34 +0300

    27 rabotaet

---
+++ dz4-zadacha27/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int res = 0;
+while (num>0)
+{
+    res=num%10;
+    sum=sum+res;
+    num/=10;
+}
+Console.WriteLine($"Сумма цифр числа = {sum}");
+
+
+
+
+
+
+
+
