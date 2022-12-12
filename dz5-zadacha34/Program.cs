878d14251449a208b63147fcaa77c5e4f1765095
AlekseyKondratew <alekseykondratew@mail.ru>
2022-12-12 14:36:19 +0300

    34 rabotaet

---
+++ dz5-zadacha34/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Задайте количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+int count = 0;
+for (int index = 0; index < n; index++)
+{
+    array [index]= new Random().Next(100,1000);
+    if (array[index] % 2 == 0)
+    count++;
+
+}
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine($"Количество чётный элементов в массиве: {count}");
