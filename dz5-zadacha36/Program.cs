e40de04c044f036c5ae6857142753b91dae25004
AlekseyKondratew <alekseykondratew@mail.ru>
2022-12-12 14:48:46 +0300

    36 rabotaet

---
+++ dz5-zadacha36/Program.cs
@@ -0,0 +1,11 @@
+﻿int [] array = new int [ new Random().Next(1,5)];
+int sum = 0;
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = new Random().Next(0,100);
+    if(index % 2 > 0)
+       sum += array[index];
+}
+Console.WriteLine(string.Join ("", array));
+Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
+
