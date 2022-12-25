6b2fc06ee37b362da7024b36963eef12258770a1
AlekseyKondratew <alekseykondratew@mail.ru>
2022-12-25 22:22:02 +0300

    Вторая часть

--- Final task/Program.cs
+++ Final task/Program.cs
@@ -1,3 +1,14 @@
 ﻿string[] arr1 = new string[5] {"Russ", "55", "Geek", "You", "Brains"};
 string[] arr2 = new string[arr1.Length];
-
+void SecondArrWithIF(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
